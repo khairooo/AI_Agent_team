8390f6b1b364e5e201f306f6abc07abb67e88bd5
Khireddine MAMMAR <khairooo.mammar@gmail.com>
2024-12-29 09:50:39 -0600

    agents & Readme Adjusts

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # AI_Agent_team
 A project to create an AI team Agent using Phidata
 
-- 1) installing packages openai, duckduckgo, phidata
-- 2) Creating .env file 
+- 1) install packages openai, duckduckgo, phidata..
+- 2) Creat4 .env file 
+- 3) Set the model varaible "groq_model"
+- 4) Create the first Agent "web_agent" for the web search 
+- 5) Create the second Agent "data_analysis" and adding the data path to analysis
+- 6) Create the third Agent (teeam agent) to do the task with the Instructions and responses 
--- agent.py
+++ agent.py
@@ -6,6 +6,8 @@ from phi.model.groq import Groq
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from phi.agent.duckdb import DuckDbAgent
+import json
 
 # Set your API key as an environment variable
 # Example: export GROQ_API_KEY="your_api_key"
@@ -20,32 +22,38 @@ groq_model = Groq(id="llama3-groq-70b-8192-tool-use-preview", api_key=GROQ_API_K
 # Agent 1
 web_agent = Agent(
     name="Web Agent",
-    role="Search the web for information quality Engineering Datasets, better to be csv files format",
+    role="Generate a quality Engineering Datasets, better to be csv files format",
     model=groq_model,
     tools=[DuckDuckGo()],
-    instructions=["Always include sources"],
+    instructions=["Not necessarly includes sources"],
     show_tool_calls=True,
     markdown=True,
 )
 
 # Agent 2
-finance_agent = Agent(
-    name="Quality Engineering Agent",
-    role="Get Quality Engineering data to do CPK, PK, and ANOVA is possible",
+data_analyst = DuckDbAgent(
     model=groq_model,
-    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True)],
-    instructions=["Use tables to display data, python pandas DataFrames if possible"],
-    show_tool_calls=True,
+    semantic_model=json.dumps(
+        {
+            "tables": [
+                {
+                    "name": "Quality Engineering Analysis table",
+                    "description": "Contains information about Product Quality.",
+                    "path": "https://www.kaggle.com/competitions/bosch-production-line-performance/data?select=train_numeric.csv.zip",
+                }
+            ]
+        }
+    ),
     markdown=True,
 )
 
 # Agent 3 (Team)
 agent_team = Agent(
-    team=[web_agent, finance_agent],
+    team=[web_agent, data_analyst],
     model=groq_model,
-    instructions=["Always include sources", "Use tables to display data"],
+    instructions=[ "Use tables to display data"],
     show_tool_calls=True,
     markdown=True,
 )
 
-agent_team.print_response("Summarize and Do Data Analysis related to Quality Engineering and interpret the results obtained: Specifically CPK,CP, ANOVA test if needed for any company in your choice", stream=True)
+agent_team.print_response("Summarize and Do Data Analysis related to Quality Engineering and interpret the results obtained: Specifically CPK,CP, ANOVA test if needed ", stream=True)
